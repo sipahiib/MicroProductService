19e48b67436db670f3b1800e52611fa6bde7f705
İbrahim <73345010+sipahiib@users.noreply.github.com>
2023-12-21 15:05:53 +0300

    First commit

---
+++ question-service/src/main/java/com/springboot/questionservice/controller/QuestionController.java
@@ -0,0 +1,51 @@
+package com.springboot.questionservice.controller;
+
+import com.springboot.questionservice.model.Question;
+import com.springboot.questionservice.model.QuestionWrapper;
+import com.springboot.questionservice.model.Response;
+import com.springboot.questionservice.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions()
+    {
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCategory(@PathVariable String category)
+    {
+        return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestion(@RequestBody Question question)
+    {
+        return questionService.addQuestion(question);
+    }
+
+    @GetMapping("generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz(@RequestParam String category, @RequestParam int numberofquestions){
+        return questionService.getAllQuestionsForQuiz(category, numberofquestions);
+    }
+
+    @PostMapping("getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds){
+        return questionService.getQuestionsFromId(questionIds);
+    }
+
+    @PostMapping("getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses){
+        return questionService.getScore(responses);
+    }
+}
